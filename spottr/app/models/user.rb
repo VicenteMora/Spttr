2ff7b8653e3ee938589f9562efa369d0351abf8a
Vicente Mora <vicente.mora@icloud.com>
2016-02-21 00:20:15 -0500

    added devise user views

--- spottr/app/models/user.rb
+++ spottr/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :messages
+	has_one :profile
+	has_many :matches, through: :match_associations 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
