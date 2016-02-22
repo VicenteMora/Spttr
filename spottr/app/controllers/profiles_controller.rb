5e76eb95959a61aba261c7418699e6def730bdc1
Vicente Mora <vicente.mora@icloud.com>
2016-02-22 15:09:02 -0500

    fixed routing for profile controller and linked user to profile

--- spottr/app/controllers/profiles_controller.rb
+++ spottr/app/controllers/profiles_controller.rb
@@ -4,5 +4,38 @@ class ProfilesController < ApplicationController
 		@profile = Profile.new
 	end
 
+	def show
+		@profile = Profile.find_by(id: params[:id])
+	end
+
+	def edit
+		@profile = Profile.find_by(id: params[:id])
+	end
+
+
+	def create
+		@user = current_user
+    	@profile = Profile.create(profile_params)
+    	@user.profile = @profile
+    unless @user.profile
+      render 'new'
+    else
+     redirect_to '/dashboard'
+    end
+  end
+
+  	def destroy
+  		@profile = Profile.find_by(id: params[:id])
+  		if @profile
+  		@profile.destroy
+  		redirect_to '/'
+  	    else 
+  		redirect_to '404'
+  		end
+  	end
+  	private
 	
+	def profile_params
+  	params.require(:profile).permit(:first_name, :last_name, :availability, :gym, :gender, :fitness_level, :over_18, :headshot_url)
+  	end	
 end
--- spottr/config/routes.rb
+++ spottr/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/' => "home#home"
+  get '/dashboard' => "home#home"
 
   resources :profiles
 end
