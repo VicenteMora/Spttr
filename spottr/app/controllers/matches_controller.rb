6b185e853d5c87c37c40928d54e4d808d4937b5d
Vicente Mora <vicente.mora@icloud.com>
2016-02-23 14:32:46 -0500

    fixed profile match issue

--- spottr/app/controllers/matches_controller.rb
+++ spottr/app/controllers/matches_controller.rb
@@ -5,8 +5,8 @@ class MatchesController < ApplicationController
 		availability = current_user.profile.availability
 		fitness_level = current_user.profile.fitness_level
 		over_18 = current_user.profile.over_18
-		
-		 @match = Profile.where(gym: gym).where(availability: availability).where(fitness_level: fitness_level).where(over_18: over_18)
+		@match = Profile.where(gym: gym).where(availability: availability).where(fitness_level: fitness_level).where(over_18: over_18)
+    	@match.delete(current_user)
     end
 
 
